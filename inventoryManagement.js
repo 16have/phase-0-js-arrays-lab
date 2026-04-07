b6fcd3622378acae3eeb3752e3efb0cbc6407ecf
kelvin <joer88544@gmail.com>
2026-04-07 13:00:59 +0300

    submite

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,36 @@
 // Write your code here
+// Initialize the products array
+const products = [
+  "Laptop",
+  "Smartphone",
+  "Headphones",
+  "Monitor"
+];
 
+// Log the first product
+function logFirstProduct() {
+  console.log(products[0]);
+}
+function addProduct(product) {
+  products.push(product);
+  return products;
+}
+addProduct("Keyboard");
+
+function updateProductName(index, newName) {
+    products[index] = newName; 
+    return products;
+}
+
+const updated = updateProductName(1, "Tablet");
+console.log(updated);
+
+function removeLastProduct() {  products.pop();
+  return products;
+}
+const removed = removeLastProduct();
+console.log(removed); 
+console.log(products);
 
 
 // Export the necessary parts for testing
